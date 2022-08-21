ba14c3493b996287c6473cd20acc836e950e71a3
Antony <58532577+1tuz@users.noreply.github.com>
2022-08-21 21:25:51 +0300

    Update and rename setup to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
+# Setup permission
 termux-setup-storage
-pkg get wget curl fakeroot python python2 python3
+# Install musthave packages
+pkg update && pkg upgrade && pkg install wget curl fakeroot python python2 python3 figlet x11-repo tsu root-repo
+# Install awesome zsh with best setup
+git clone --depth=1 https://github.com/mayTermux/myTermux.git && cd myTermux && export COLUMNS LINES && ./install.sh
+# Add some aliases to zsh
 echo "alias upg='apt update & apt upgrade'" & "alias debug='adb-devices'" & "alias backup='tar -zcf /sdcard/termux-backup.tar.gz -C /data/data/com.termux/files ./home ./usr
 '" & "alias restore='tar -zxf /sdcard/termux-backup.tar.gz -C /data/data/com.termux/files --recursive-unlink --preserve-permissions
 '">> ~/.zshrc
