2479eb30364fb82f959207af608b77131e452552
Antony <58532577+1tuz@users.noreply.github.com>
2022-09-25 20:01:09 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # Setup storage permission
 termux-setup-storage
 # Install musthave packages
-pkg update && pkg upgrade && pkg install wget curl fakeroot python python2 python3 figlet x11-repo tsu root-repo
+pkg update && pkg upgrade && pkg install wget curl fakeroot python python2 python3 figlet x11-repo tsu root-repo && echo "All must-have packages has been installed!" || echo "Oops! Something is wrong =("
 # Install awesome zsh with best setup
 git clone --depth=1 https://github.com/mayTermux/myTermux.git && cd myTermux && export COLUMNS LINES && ./install.sh
 # Add some aliases to zsh
