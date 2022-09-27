00603b85e0afacec5fa0fd2dc1ad2d99badc9910
Antony <58532577+1tuz@users.noreply.github.com>
2022-09-27 09:21:12 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ termux-setup-storage
 # Install musthave packages
 pkg update && pkg upgrade && pkg install wget curl fakeroot python python2 python3 figlet x11-repo tsu root-repo && echo "All must-have packages has been installed!" || echo "Oops! Something is wrong =("
 # Install awesome shell(zsh) with the best setup
-git clone --depth=1 https://github.com/mayTermux/myTermux.git && cd myTermux && export COLUMNS LINES && yes | ./install.sh
+cd && git clone --depth=1 https://github.com/mayTermux/myTermux.git && cd myTermux && export COLUMNS LINES && yes | ./install.sh
 # Add some aliases to zsh
 echo "alias upg='apt update & apt upgrade'" & "alias debug='adb-devices'" & "alias backup='tar -zcf /sdcard/termux-backup.tar.gz -C /data/data/com.termux/files ./home ./usr
 '" & "alias restore='tar -zxf /sdcard/termux-backup.tar.gz -C /data/data/com.termux/files --recursive-unlink --preserve-permissions
