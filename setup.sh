965726f99bda2fc26aeccb852d3499e1dccf076a
Antony <58532577+1tuz@users.noreply.github.com>
2023-01-25 23:48:04 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,32 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 # Setup storage permission
-termux-setup-storage
-# Install musthave packages
-pkg update && pkg upgrade && pkg install wget curl fakeroot python python2 python3 figlet x11-repo tsu root-repo && echo "All must-have packages has been installed!" || echo "Oops! Something is wrong =("
+if termux-setup-storage; then
+  echo "Storage permission has been set up!"
+else
+  echo "Error: Failed to set up storage permission!"
+  exit 1
+fi
+
+# Install must-have packages
+if pkg update && pkg upgrade -y && pkg install -y wget curl fakeroot python python2 python3 figlet x11-repo tsu root-repo; then
+  echo "All must-have packages have been installed!"
+else
+  echo "Error: Failed to install packages!"
+  exit 1
+fi
+
 # Install awesome shell(zsh) with the best setup
-cd && git clone --depth=1 https://github.com/mayTermux/myTermux.git && cd myTermux && export COLUMNS LINES && ./install.sh
+if cd && wget https://raw.githubusercontent.com/mayTermux/myTermux/master/install.sh && chmod +x install.sh && ./install.sh; then
+  echo "Zsh has been installed and configured!"
+else
+  echo "Error: Failed to install Zsh!"
+  exit 1
+fi
+
 # Add some aliases to zsh
-echo "alias upg='apt update & apt upgrade'" & "alias debug='adb-devices'" & "alias backup='tar -zcf /sdcard/termux-backup.tar.gz -C /data/data/com.termux/files ./home ./usr
-'" & "alias restore='tar -zxf /sdcard/termux-backup.tar.gz -C /data/data/com.termux/files --recursive-unlink --preserve-permissions
-'">> ~/.zshrc
+echo "alias upg='apt update && apt upgrade'" >> ~/.zshrc
+echo "alias debug='adb-devices'" >> ~/.zshrc
+echo "alias backup='tar -zcf /sdcard/termux-backup.tar.gz -C /data/data/com.termux/files ./home ./usr'" >> ~/.zshrc
+echo "alias restore='tar -zxf /sdcard/termux-backup.tar.gz -C /data/data/com.termux/files --recursive-unlink --preserve-permissions'" >> ~/.zshrc
+echo "Additional aliases have been added to your zsh configuration."
